0ea8cf77d9b0aa733983a82eee26977c7818f274
fengfeng <1183931388@qq.com>
2020-06-03 21:51:33 +0800

    'tj'

--- week08/match.js
+++ week08/match.js
@@ -1,9 +1,30 @@
 // 该代码，可在week07 toy-brower中进行测试
 // server.js 中style 已改成#container .c1#dd
+// 该函数只处理了，id、class、空格和tag
 
 function match(element, selector) {
     if (!selector || !element.attributes)
     return false;
+    // 通过Tokenizer 库解析selector，获得一个形如一下格式的数据结构
+    // {
+    // type: "selectors",
+    // nodes: [
+    //     {
+        //     type: "selector",
+        //     nodes: [
+        //         { type: "element", name: "a" },
+        //         { type: "id", name: "content" },
+        //         { type: "class", name: "active" },
+        //         { type: "operator", operator: ">", before: " ", after: " " },
+        //         { type: "element", name: "div" },
+        //         { type: "pseudo-element", name: "first-line" },
+        //         { type: "spacing", value: " " },
+        //         { type: "attribute", content: "data-content" },
+        //     ]
+    //     }
+    // ]}
+    // 从后往头，一个个解析选择器是否与当前元素的，各种属性相匹配
+    // 需要注意的是通过最外层的nodes得到的是选择器列表，也就是逗号分割的选择器；
     let parseRules = Tokenizer.parse(selector)
   
     let ruleGroup = parseRules.nodes
